e8799e63510b24e0ecf595b55c1f8467454a3450
AMNshaw <rosel94638@gmail.com>
2022-12-13 12:45:08 +0800

    make all

--- node/motor_speed_generator.cpp
+++ node/motor_speed_generator.cpp
@@ -4,8 +4,7 @@
 #include <geometry_msgs/Point.h>
 #include <std_msgs/Int32.h>
 #include <cmath>
-#include "OsqpEigen/OsqpEigen.h"
-#include <Eigen/Dense>
+#include <eigen3/Eigen/Dense>
 
 using namespace std;
 
@@ -21,51 +20,61 @@ private:
     float kp_omega;
     float kd_omega;
     geometry_msgs::TwistStamped desired_vel;
-    ros::Subscriber desired_vel;
+    ros::Subscriber desired_vel_sub;
 public:
     Mobile();
-    Mobile();
+    Mobile(ros::NodeHandle nh, string desired_vel_subTopic);
+    void setMobileParam(float w, float r);
+    void setPdCtrlParam(float kp, float kd);
     void desired_vel_cb(const geometry_msgs::TwistStamped::ConstPtr& msg);
     void computeWheelSpd();
+};
+
+Mobile::Mobile(ros::NodeHandle nh, string desired_vel_subTopic)
+{
+    desired_vel_sub = nh.subscribe<geometry_msgs::TwistStamped>(desired_vel_subTopic, 10, &Mobile::desired_vel_cb, this);
+    width = 1;
+    wheelRadius = 0.0325;
+    kp_omega = 1;
+    kd_omega = 1;
+}
+
+void Mobile::desired_vel_cb(const geometry_msgs::TwistStamped::ConstPtr& msg)
+{  
+    desired_vel.twist.linear.x = msg->twist.linear.x;
+    desired_vel.twist.linear.y = msg->twist.linear.y;
+    computeWheelSpd();
 }
 
 void Mobile::computeWheelSpd()
 {
     float v_norm = sqrt(pow(desired_vel.twist.linear.x, 2) + pow(desired_vel.twist.linear.y, 2));
     theta = acos(desired_vel.twist.linear.x/v_norm);
+
+    // PD control of self spinning
     omega_self = kp_omega*theta;
 
-    omega_L = (v_norm-omega_self)*width/2;
-    omega_R = (v_norm+omega_self)*width/2
+    omega_L = (v_norm-omega_self*width/2)/wheelRadius;
+    omega_R = (v_norm+omega_self*width/2)/wheelRadius;
 }
 
+void Mobile::setMobileParam(float w, float r){width = w; wheelRadius = r;}
+void Mobile::setPdCtrlParam(float kp, float kd){kp_omega = kp; kd_omega = kd;};
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "motor_speed_generation");
     ros::NodeHandle nh;
-
-    ros::Publisher local_vel_pub = nh.advertise<geometry_msgs::TwistStamped>("cmd/vel", 2);
-
     ros::Rate rate(100);
 
-    CBF cbf(nh, "/aprilTag_pos");
-    cbf.setCBFparam(0.3, 0.2, 0.6);
-    geometry_msgs::TwistStamped desired_vel;
-    geometry_msgs::TwistStamped desired_vel_raw;
-    desired_vel.twist.linear.x = 0;
-    desired_vel.twist.linear.y = 0;
-    desired_vel_raw.twist.linear.x = 0;
-    desired_vel_raw.twist.linear.y = 0;
+    Mobile car(nh, "/cmd/vel");
+    car.setMobileParam(15, 0.0325);
+    car.setPdCtrlParam(1, 1);
 
-    
     while(ros::ok())
     {
-        cbf.QPsolve_vel(desired_vel_raw , &desired_vel);
         ros::spinOnce();
         rate.sleep();
-        
-        cout << desired_vel << endl;
     }
     
     return 0;
