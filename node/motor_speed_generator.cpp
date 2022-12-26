4f6dcbcef9b5a7862ee61c36d0a3f5a6315899e1
AMNshaw <rosel94638@gmail.com>
2022-12-26 17:24:38 +0800

    debug

--- node/motor_speed_generator.cpp
+++ node/motor_speed_generator.cpp
@@ -66,16 +66,19 @@ void Mobile::computeAcc()
 
 void Mobile::desired_vel_cb(const geometry_msgs::TwistStamped::ConstPtr& msg)
 {   
+    /*
     desired_vel.twist.linear.x = msg->twist.linear.x;
     desired_vel.twist.linear.y = msg->twist.linear.y;
     computeAcc();
     current_vel.twist.linear.x = current_vel.twist.linear.x + acc.x*delta_t;
     current_vel.twist.linear.y = current_vel.twist.linear.y + acc.y*delta_t;
     computeWheelSpd();
-
-    if(abs(omega_L) < 0.01 && abs(omega_R) < 0.01)
-        omega_L = omega_R = 0;
-
+    */
+    current_vel.twist.linear.x = msg->twist.linear.x;
+    current_vel.twist.linear.y = msg->twist.linear.y;
+    computeAcc();
+    computeWheelSpd();
+    
     cout << "L: " << omega_L << " R: " << omega_R << endl;
 
        
